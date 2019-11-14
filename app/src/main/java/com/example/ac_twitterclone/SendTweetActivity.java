bbab75227646b80c1ca1a9244963679638d85758
Sundeep <skandoi30@gmail.com>
2019-11-14 16:08:58 +0530

    Final Commit:  TwitterCloneCompleted

--- app/src/main/java/com/example/ac_twitterclone/SendTweetActivity.java
+++ app/src/main/java/com/example/ac_twitterclone/SendTweetActivity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class SendTweetActivity extends AppCompatActivity {
+
+public class SendTweetActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText edtMyTweet;
+    private Button btnViewTweets;
+
+    private ListView listViewTweets;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +37,18 @@ public class SendTweetActivity extends AppCompatActivity {
         setContentView(R.layout.activity_send_tweet);
 
         edtMyTweet = findViewById(R.id.xedtTweet);
+        btnViewTweets = findViewById(R.id.xbtnViewTweets);
+        listViewTweets = findViewById(R.id.xlistViewTweets);
+
+        btnViewTweets.setOnClickListener(this);
+
+//        HashMap<String,Integer> hashMap = new HashMap<>();
+//        hashMap.put("key1",1);
+//        hashMap.put("key2",2);
+//        FancyToast.makeText(this,hashMap.get("key2")+"", FancyToast.LENGTH_LONG,FancyToast.WARNING,false).show();
 
 
-    }
+    }   // end of onCreate
 
     public void sendTweet  (View view){
 
@@ -50,5 +72,38 @@ public class SendTweetActivity extends AppCompatActivity {
                 progressDialog.dismiss();
             }   // end of done
         });     // end of saveInBackground
-    }
+    }   // end of sendTweet
+
+    @Override
+    public void onClick(View view) {
+
+        final ArrayList<HashMap<String,String>> tweetList = new ArrayList<>();
+        final SimpleAdapter adapter = new SimpleAdapter(SendTweetActivity.this,tweetList,
+                android.R.layout.simple_list_item_2,new String[]{"tweetUserName","userTweet"},
+                new  int[]{android.R.id.text1,android.R.id.text2});
+
+        try {
+            ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("MyTweets");
+            parseQuery.whereContainedIn("user",ParseUser.getCurrentUser().getList("followList"));
+            parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> objects, ParseException e) {
+                    if (objects.size() >0 && e== null ){
+                        for (ParseObject tweetObject : objects){
+                            HashMap<String,String> userTweet = new HashMap<>();
+                            userTweet.put("tweetUserName",tweetObject.getString("user")+"\t"+ tweetObject.getCreatedAt());
+                            userTweet.put("userTweet",tweetObject.getString("tweet"));
+                            tweetList.add(userTweet);
+                        }   // end of for
+                        listViewTweets.setAdapter(adapter);
+                    }   // end of if
+                }   // end of done
+            }); // end of findInBackground
+        }catch (Exception e){
+            e.printStackTrace();
+        }   // end of try/catch
+
+
+
+    }   // end of onClick
 }
